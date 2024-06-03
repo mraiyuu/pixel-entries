a1c7098018beb13fc2982374a2f44bc7c6c28524
mraiyuu <munenedenis618@gmail.com>
2024-06-03 13:07:20 +0300

    layout structure

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,19 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+
+<div class="self-start text-sm">Laracasts</div>
+<div class="font-bold py-8">
+    <h3>Backend developer</h3>
+    <p>Full Time -from $120,000</p>
+</div>
+<div class="flex justify-between items-center">
+
+    <div>
+       <x-tag>Tag</x-tag>
+       <x-tag>Tag</x-tag>
+       <x-tag>Tag</x-tag>
+
+    </div>
+    <img src="http://placehold.it/42/42" alt="image" class="rounded-xl"/>
+</div>
+
+</div>
