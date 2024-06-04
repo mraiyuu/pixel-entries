b91feee86ff078077567043885dfb2f3dd4e6743
mraiyuu <munenedenis618@gmail.com>
2024-06-04 11:02:53 +0300

    UI work in progress

--- resources/views/components/panel.blade.php
+++ resources/views/components/panel.blade.php
@@ -1,1 +1,5 @@
-<div class="p-4 bg-white/5 rounded-xl flex gap-x-6 border border-transparent hover:border-blue-800 group transition-colors duration-600">{{ $slot}}</div>
+@php
+ $classes="p-4 bg-white/5 rounded-xl border border-transparent hover:border-blue-800 group transition-colors duration-600"
+@endphp
+
+<div {{ $attributes(['class'=>$classes])}} >{{ $slot}}</div>
