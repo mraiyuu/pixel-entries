13d975ed17f3afc78db23297cc3ac4f3d2755947
mraiyuu <munenedenis618@gmail.com>
2024-06-10 21:40:00 +0300

    factories and migrations created

--- database/migrations/2024_06_10_180556_create_jobs_table.php
+++ database/migrations/2024_06_10_180556_create_jobs_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Employer;
 
 return new class extends Migration
 {
@@ -13,6 +14,13 @@ return new class extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Employer::class);
+            $table->string('title');
+            $table->string('salary');
+            $table->string('location');
+            $table->string('schedule')->default('Full Time');
+            $table->string('url');
+            $table->boolean('featured')->default('false');
             $table->timestamps();
         });
     }
